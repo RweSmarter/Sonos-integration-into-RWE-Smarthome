1fda93db3ebfe524acb8a922aa50d90b45a0e9a4
Smarter <RweSmarter@users.noreply.github.com>
2014-03-25 07:27:39 +0100

    Update mySonosScripts.php
    Eine neue Funktion wurde hinzugefügt. Ein HKT kann nun verwendet werden um vordefinierte Radiosender einzustellen.
    Kurzanleitung:
    1.man definiert seine Radiosender in der Config.ini für jede Raumthemperatur. Bei mir sind 22.5 und 19.0 nicht definiert. Dies sind die Defaulttemperaturen und bei diesen Themperaturen können Playlisten abgespielt werden
    2. in RWE SM legt man sich eine ZV an (siehe Anhang) die folgenden Aufbau haben muss.
    Sonos_Radio_Bad. Es wird dann die Funktion Radio aufgerufen mit dem Argument Bad d.h. der HKT im im Raum BAD kann nun den Player in der Zone Bad abspielen. Raum- und Zonennamen müssen identisch sein. Man kann die ZV mit einem Zeitprofil versehen, sodass die Regelung am HKT nur in dieser Zeit gültig ist z.B. Morgen von 7:00 - 9:00.

--- mySonosScripts.php
+++ mySonosScripts.php
@@ -1,14 +1,46 @@
 <?php 
 // Smarters user defined functions
-function StopAll()
-{ 	ini_set('allow_url_fopen ','ON');
-	global $configIni;# content of config.ini file
-	$allZones = $configIni['SonosZones'];
+function StopAll($parameter)
+{ 	global $configIni;# content of config.ini file
+	$allZones = $configIni['SonosZones'];  # read Sonos zone from config.ini
 	foreach($allZones as $index => $zone)
 	 {
-  		$ipAddress = $allZones[$index];
-		$sonos = new PHPSonos($ipAddress); //Sonos IPAdresse
-  		$sonos->Stop();
+  		$ipAddress = $allZones[$index];    # read Sonos zones from config ini
+		$sonos = new PHPSonos($ipAddress); # init Sonos speaker
+  		$sonos->Stop();  
   	 }
 }
+
+function Radio($parameter)
+
+{   global $configIni;# content of config.ini file
+	global $LDSARR;
+	global $LDS;
+	global $newLine;  # used for debugging
+	$RWEroom=strtoupper($parameter); # RWE room name only capitle letters, Sonos room name case sensitive
+	//---------------------foreach logical device
+	foreach ($LDSARR as $LD) {
+		$LID         = $LD["LID"]->__toString();
+		$ROOM        = $LDS["$LID"][4];
+		$LDNAME      = $LDS["$LID"][0];
+		$PTTMP       = isset ($LD["PtTmp"])            ? $LD["PtTmp"]->__toString()            : null;  // Rst: RoomTemperatureActuatorState    Actor: Temperature
+		if (isset($PTTMP) && $ROOM ==$RWEroom)  
+		{
+			$RadioStation = isset($configIni['SonosRadiosStation'][$PTTMP]) ? $configIni['SonosRadiosStation'][$PTTMP] : ""; # read radio station from config.ini
+			$RadioURL     = isset($configIni['SonosRadiosURL'] [$PTTMP]) ? $configIni['SonosRadiosURL'] [$PTTMP] : "";    # read streaming URL from cnfig.ini
+			$ipAddress    = $configIni['SonosZones'][$parameter];     # replace zone name with speaker IP address from config.ini 
+			$sonos = new PHPSonos($ipAddress);                        # init Sonos speaker
+			$radiosender = $sonos->GetPositionInfo();			      # get Sonos info
+			$senderurl = $radiosender["URI"];                         # current radio station
+			$save_Status = $sonos->GetTransportInfo();                # save_Status //1 = PLAYING //2 = PAUSED_PLAYBACK //3 = STOPPED
+			if ($senderurl !== $RadioURL and $RadioURL !== "" and $save_Status == 1 )  # if new station & url not empty (default temperatur used for playlists) and player is playing
+			{
+				$sonos->SetRadio($RadioURL, $RadioStation);               # Play radio
+				$sonos->Play();                                           
+				echo "Option3: ZV= '".$LDNAME. "' => Radio= ". $PTTMP . " Grad   =>Station= " . $RadioStation . $newLine;
+			}
+		}
+	}// end foreach
+}
+
 ?>
